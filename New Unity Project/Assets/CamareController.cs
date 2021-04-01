9bb98eaa51af354fa98259b66e10cda1343a343d
jnghy <78326562+jnghy@users.noreply.github.com>
2021-04-01 10:32:17 +0800

    add dynamic camera
    still needs to have constraints so it won't show the background

---
+++ New Unity Project/Assets/CamareController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamareController : MonoBehaviour
+{
+    private Transform player;
+    private Vector3 offset;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+        offset = new Vector3(3, 2, -10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z);
+    }
+}
